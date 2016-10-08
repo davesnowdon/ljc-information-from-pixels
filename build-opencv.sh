101a7b891d2a06ae0600628295a6334372186eb5
Dave Snowdon <dave@davesnowdon.com>
2016-10-08 11:20:31 +0100

    snapshot

--- build-opencv.sh
+++ build-opencv.sh
@@ -1,21 +1,37 @@
 #! /bin/bash
 
 # Open CV pre-requisites
+# cmake
 # ant 1.9.6
+
+BUILDOPTS="-DBUILD_SHARED_LIBS=OFF"
+
+# disable video on MacOS since QTKit has been removed from recent versions of MacOS
+BUILDOS=$(uname -s)
+if [ "$BUILDOS" == "Darwin" ]; then
+    BUILDOPTS="${BUILDOPTS} -DBUILD_opencv_videoio=OFF"
+    echo "Disabling video"
+fi
+
 
 export OPENCV_HOME="$(pwd)/opencv-3.1.0"
 cd $OPENCV_HOME
 mkdir build
 cd build
-cmake -DBUILD_SHARED_LIBS=OFF ..
+cmake ${BUILDOPTS} ..
 make 
 
 mkdir ../../lib
+cp bin/opencv-310.jar ../../lib/
 cp lib/libopencv_java310.so ../../lib/
-mv bin/opencv-310.jar ../../lib/
 cd ../../lib/
-mkdir -p native/linux/x86_64
-cp libopencv_java310.so native/linux/x86_64/
+if [ "$BUILDOS" == "Darwin" ]; then
+    mkdir -p native/macosx/x86_64
+    cp libopencv_java310.so native/macosx/x86_64/
+else
+    mkdir -p native/linux/x86_64
+    cp libopencv_java310.so native/linux/x86_64/
+fi
 jar -cMf opencv-native-310.jar native
 
 # needs leiningen
